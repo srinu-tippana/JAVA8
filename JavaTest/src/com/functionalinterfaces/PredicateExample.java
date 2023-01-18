35d952c5cf5ff39403cb82f841c019da14fe010c
srinu-tippana <svas7246@gmail.com>
2023-01-19 00:30:01 +0530

    updated folder name

--- JavaTest/src/com/functionalinterfaces/PredicateExample.java
+++ JavaTest/src/com/functionalinterfaces/PredicateExample.java
@@ -9,13 +9,23 @@ public class PredicateExample {
 
    static Predicate<Integer> predicate1 =(i)->i%2==0;
 
+   static Predicate<Integer> predicate2 =(i)->i%5==0;
+
+    /**
+     * This method is to test the combination of predicate's test and  functions  Declared as static so that can be called from the context 
+     */
    public static void predicateAnd(){
 
+       System.out.println(predicate1.and(predicate2).test(8));
    }
     public static void main(String[] args) {
 
         System.out.println(predicate.test(3));
        System.out.println(predicate1.test(4 ));
+
+
+
+
 
 
     }
