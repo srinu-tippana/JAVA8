b1affad412122a9fa8e62506a2d95e00a893b415
srinu-tippana <svas7246@gmail.com>
2023-01-19 00:41:18 +0530

    or operation using predicate

--- JavaTest/src/com/functionalinterfaces/PredicateExample.java
+++ JavaTest/src/com/functionalinterfaces/PredicateExample.java
@@ -12,16 +12,20 @@ public class PredicateExample {
    static Predicate<Integer> predicate2 =(i)->i%5==0;
 
     /**
-     * This method is to test the combination of predicate's test and  functions  Declared as static so that can be called from the context 
+     * This method is to test the combination of predicate's test and  functions  Declared as static so that can be called from the context
      */
    public static void predicateAnd(){
 
-       System.out.println(predicate1.and(predicate2).test(8));
+       System.out.println(predicate1.and(predicate2).test(10));
+       System.out.println(predicate1.or(predicate2).test(4));
    }
     public static void main(String[] args) {
 
         System.out.println(predicate.test(3));
        System.out.println(predicate1.test(4 ));
+
+
+       predicateAnd();
 
 
 
