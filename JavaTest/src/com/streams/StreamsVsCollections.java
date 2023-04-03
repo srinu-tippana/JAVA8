ead4d52c3a28b1f442cab1e095ed29240acc9d37
srinu-tippana <svas7246@gmail.com>
2023-04-03 23:45:57 +0530

    Example o Streams

--- JavaTest/src/com/streams/StreamsVsCollections.java
+++ JavaTest/src/com/streams/StreamsVsCollections.java
@@ -1,7 +1,15 @@
 package com.streams;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StreamsVsCollections {
     public static void main(String[] args) {
-        
+
+        List<String> arrayList = new ArrayList<String>();
+        arrayList.add("srinu");
+        arrayList.add("Code");
+
+        System.out.println(arrayList.stream().count());
     }
 }
